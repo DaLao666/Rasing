f45b07aab248f9247376bf127550b7b08ec82e23
hjy <2171318136@qq.com>
2017-05-12 12:52:26 +0800

    前台网页

---
+++ BLOG/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLOG.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            return View();
+        }
+        public ActionResult register()
+        {
+            return View();
+        }
+        public ActionResult a1()
+        {
+            return View();
+        }
+        public ActionResult a2()
+        {
+            return View();
+        }
+        public ActionResult a3()
+        {
+            return View();
+        }
+        public ActionResult a4()
+        {
+            return View();
+        }
+        public ActionResult a5()
+        {
+            return View();
+        }
+        public ActionResult a6()
+        {
+            return View();
+        }
+    }
+}
---
+++ BLOG/Controllers/a1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLOG.Controllers
+{
+    public class a1Controller : Controller
+    {
+        // GET: a1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
